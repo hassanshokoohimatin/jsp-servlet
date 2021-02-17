663774543531737726f618bd5827bef593bbcce4
asus <hassan.shokoohimatin@gmail.com>
2021-02-17 17:29:37 +0330

    user repository added

---
+++ src/service/UserRepository.java
@@ -0,0 +1,23 @@
+package service;
+
+import config.CrudRepository;
+import model.User;
+
+public class UserRepository extends CrudRepository<User> {
+
+    private static UserRepository userRepository;
+
+    private UserRepository(){
+    }
+
+    public static UserRepository getInstance(){
+        if (userRepository == null)
+            userRepository = new UserRepository();
+        return userRepository;
+    }
+
+    @Override
+    protected Class<User> getEntityClass() {
+        return User.class;
+    }
+}
