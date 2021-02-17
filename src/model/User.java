889099ead717e11fa5b8d9ac24278df7ce933062
asus <hassan.shokoohimatin@gmail.com>
2021-02-17 17:07:23 +0330

    model created

--- src/model/User.java
+++ src/model/User.java
@@ -10,5 +10,62 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
+    private String firstName;
+    private String lastName;
+    private String nationalCode;
+    private String mobile;
+
+    //Constructors, setters and getters
+
+    public User() {
+    }
+
+    public User(String firstName, String lastName, String nationalCode, String mobile) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nationalCode = nationalCode;
+        this.mobile = mobile;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getNationalCode() {
+        return nationalCode;
+    }
+
+    public void setNationalCode(String nationalCode) {
+        this.nationalCode = nationalCode;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
 }
